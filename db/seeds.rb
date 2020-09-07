85201e9394e72c4b1803bc182aeab361a7da58f6
Mimioj101 <mimioj@gmail.com>
2020-09-07 18:30:02 -0400

    added user names column, built up users controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,17 @@
 
 
 
+i = 1
+while i < 11
+    puts 'difficulty level: ' + i.to_s
+    10.times do
+        response = HTTParty.get("https://twinword-word-association-quiz.p.rapidapi.com/type1/?area=sat&level=" + i.to_s, headers:{
+            "X-RapidAPI-Host" => "twinword-word-association-quiz.p.rapidapi.com",
+            "X-RapidAPI-Key" => ""
+        })
         response['quizlist'].each do |question|
             Question.create(related_words: question.values[0], option_1: question.values[1][0], option_2: question.values[1][1], correct_answer: question.values[2], level: 'SAT' , difficulty: i)
         end
+    end
+    i = i +1
+end
