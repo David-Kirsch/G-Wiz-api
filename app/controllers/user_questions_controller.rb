56f23786887dfe350ea476990699eecf7b4d3c43
David-Kirsch <davidjkirsch@gmail.com>
2020-09-08 17:18:46 -0400

    Added create action for userQuestions

--- app/controllers/user_questions_controller.rb
+++ app/controllers/user_questions_controller.rb
@@ -8,4 +8,14 @@ class UserQuestionsController < ApplicationController
         userQuestion = UserQuestion.find(params[:id])
         render json: userQuestion, except: [:created_at, :updated_at, :outcome]
     end
+
+    def create
+        userQuestion = UserQuestion.create!(user_question_params)
+        render json: userQuestion, except: [:created_at, :updated_at, :outcome]
+    end
+
+private
+    def user_question_params
+        params.require(:user_question).permit(:user_id, :question_id, :outcome)
+    end
 end
