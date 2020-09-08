18d87db84e049d68dd99ae9ebea8722bc7c1b336
Mimioj101 <mimioj@gmail.com>
2020-09-08 15:57:17 -0400

    user_question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,21 @@
 class QuestionsController < ApplicationController
-def index
-    
-    questions = Question.all 
-    render json: questions, except: [:created_at, :updated_at]
+    def index
+        questions = Question.all 
+        render json: questions, except: [:created_at, :updated_at]
     end
 
     def show
         question = Question.find(params[:id])
         render json: question, except: [:created_at, :updated_at]
+    end
+
+    def create
+        question = Question.create!(question_params)
+        render json: question, except: [:created_at, :updated_at]
+    end
+
+private
+    def question_params
+        params.require(:question).permit(:option_1, :option_2, :correct_answer, :level, :difficulty, related_words: [])
     end
 end
--- app/controllers/user_questions_controller.rb
+++ app/controllers/user_questions_controller.rb
@@ -1,2 +1,11 @@
 class UserQuestionsController < ApplicationController
+    def index
+        userQuestions = UserQuestion.all 
+        render json: userQuestions, except: [:created_at, :updated_at, :outcome]
+    end
+
+    def show
+        userQuestion = UserQuestion.find(params[:id])
+        render json: userQuestion, except: [:created_at, :updated_at, :outcome]
+    end
 end
